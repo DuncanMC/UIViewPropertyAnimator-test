7ec692308064f1d3f8449d77678b4caef09e4385
Duncan Champney <DChampney@wareto.com>
2016-12-09 14:33:14 -0500

    Added options to use spring damping in animation.

--- UIVIew PropertyAnimator test/ViewController.swift
+++ UIVIew PropertyAnimator test/ViewController.swift
@@ -10,7 +10,18 @@ import UIKit
 
 
 class ViewController: UIViewController {
-  
+
+  var useSpringAnimation = true
+
+  let margin: CGFloat =             30.00
+  var animationDuration =            0.50
+  var springDampingRatio: CGFloat =  0.30 {
+    didSet {
+      dampingFactorLabel.text = String(format: "%.02f", springDampingRatio)
+      dampingFactorSlider.value = Float(springDampingRatio)
+    }
+  }
+
   //MARK: - IBOutlets
   
   ///This constraint controls the center x of the image view we're animating
@@ -21,13 +32,23 @@ class ViewController: UIViewController {
   @IBOutlet weak var imageView: UIImageView!
   @IBOutlet weak var reverseButton: UIButton!
   @IBOutlet weak var animationDirectionLabel: UILabel!
+  @IBOutlet weak var dampingFactorLabel: UILabel!
+  @IBOutlet weak var dampingFactorSlider: UISlider!
+  @IBOutlet weak var useSpringAnimationSwitch: UISwitch!
   //MARK: - Properties
   
+  var viewAnimator: UIViewPropertyAnimator?
+
   ///This property keeps track of whether we're animating backwards or forwards
   var animatingBackwards: Bool = false {
     didSet {
-      viewAnimator.isReversed = animatingBackwards
-      reverseButton.isEnabled = viewAnimator.state == .active && !viewAnimator.isRunning
+      viewAnimator?.isReversed = animatingBackwards
+      if let viewAnimator = viewAnimator {
+        reverseButton.isEnabled = viewAnimator.state == .active && !viewAnimator.isRunning
+      } else {
+        reverseButton.isEnabled = false
+      }
+        
       if animatingBackwards {
         animationDirectionLabel.text = NSLocalizedString("Backward", comment: "")
       } else {
@@ -36,7 +57,6 @@ class ViewController: UIViewController {
     }
   }
   
-  var viewAnimator: UIViewPropertyAnimator!
 
   var animationIsRunning = false {
     didSet {
@@ -47,10 +67,15 @@ class ViewController: UIViewController {
         startStopButton.setTitle(NSLocalizedString("Start", comment: ""), for: .normal)
         startTimer(false)
       }
+      let canAdjustDamping = !animationIsRunning &&
+        viewAnimator?.state != .active &&
+        viewAnimator?.state != .stopped
+      
+      useSpringAnimationSwitch.isEnabled = canAdjustDamping
+      dampingFactorSlider.isEnabled = canAdjustDamping
     }
   }
   
-  let animationDuration = 2.0
   
   weak var timer: Timer?
   
@@ -62,18 +87,30 @@ class ViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    viewAnimator = UIViewPropertyAnimator(duration: animationDuration, curve: .easeInOut)
-    viewAnimator.isInterruptible = true
+    setupAnimator()
+    viewAnimator?.isInterruptible = true
+    springDampingRatio = 0.3
   }
 
   //MARK: - Custom Instance Methods
+  
+  func setupAnimator() {
+    if useSpringAnimation {
+      print("Using spring damping")
+      viewAnimator = UIViewPropertyAnimator(duration: animationDuration,
+                                            dampingRatio: springDampingRatio)
+    } else {
+      print("Using ease in/out animation")
+      viewAnimator = UIViewPropertyAnimator(duration: animationDuration, curve: .easeInOut)
+    }
+  }
   
   func startTimer(_ start: Bool) {
     if start {
       timer = Timer.scheduledTimer(withTimeInterval: 0.02,
                                    repeats: true) {
                                     timer in
-                                    var value = Float(self.viewAnimator.fractionComplete)
+                                    var value = Float(self.viewAnimator?.fractionComplete ?? 0.0)
                                     if self.animatingBackwards {
                                       value = 1 - value
                                     }
@@ -87,11 +124,21 @@ class ViewController: UIViewController {
   
   //MARK: - IBActions
 
+  @IBAction func handleDampingSlider(_ sender: UISlider) {
+    springDampingRatio = CGFloat(dampingFactorSlider.value)
+    viewAnimator = nil
+  }
+  
+  @IBAction func handleDampingSwitch(_ sender: UISwitch) {
+    useSpringAnimation = useSpringAnimationSwitch.isOn
+    setupAnimator()
+  }
+  
   @IBAction func handleSlider(_ sender: UISlider) {
     let sliderValue = CGFloat(sender.value)
-    viewAnimator.fractionComplete = sliderValue
-    if viewAnimator.isRunning {
-      viewAnimator.pauseAnimation()
+    viewAnimator?.fractionComplete = sliderValue
+    if viewAnimator?.isRunning ?? false {
+      viewAnimator?.pauseAnimation()
       animationIsRunning = false
     }
   }
@@ -102,57 +149,67 @@ class ViewController: UIViewController {
   
   @IBAction func handleStartStopButton(_ sender: UIButton) {
     
-    switch viewAnimator.state {
-    case .inactive:
-      animationSlider.isEnabled = true
-      resetButton.isEnabled = true
-      reverseButton.isEnabled = true
-
-      animationIsRunning = true
-      centerConstraint.constant = 30
-      self.view.layoutIfNeeded()
-      viewAnimator.addAnimations {
-        [weak centerConstraint, weak self] in
-        guard let strongCenterConstraint = centerConstraint,
-          let strongSelf = self else {
-            return
-        }
-        strongCenterConstraint.constant = strongSelf.view.bounds.width - 30
-        strongSelf.view.layoutIfNeeded()
-      }
-      viewAnimator.addCompletion() {
-        (animatingPosition) in
-        self.handleResetButton(nil)
-      }
-      viewAnimator.startAnimation()
-    case .active:
-      reverseButton.isEnabled = true
-      if viewAnimator.isRunning {
-        viewAnimator.pauseAnimation()
-        animationIsRunning = false
-        resetButton.isEnabled = true
-      } else {
-        viewAnimator.startAnimation()
-        reverseButton.isEnabled = !animatingBackwards
-        animationIsRunning = true
-        
-      }
-
-    case .stopped:
-      viewAnimator.startAnimation()
-      animationIsRunning = true
+    if  viewAnimator == nil {
+      setupAnimator()
     }
-
+    if let viewAnimator = viewAnimator {
+      switch viewAnimator.state {
+      case .inactive:
+        animationSlider.isEnabled = true
+        resetButton.isEnabled = true
+        reverseButton.isEnabled = true
+        
+        animationIsRunning = true
+        centerConstraint.constant = margin
+        self.view.layoutIfNeeded()
+        viewAnimator.addAnimations {
+          [weak centerConstraint, weak self] in
+          guard let strongCenterConstraint = centerConstraint,
+            let strongSelf = self else {
+              return
+          }
+          strongCenterConstraint.constant = strongSelf.view.center.y
+          strongSelf.view.layoutIfNeeded()
+        }
+        viewAnimator.addCompletion() {
+          (animatingPosition) in
+          DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            self.handleResetButton(nil)
+          }
+        }
+        viewAnimator.startAnimation()
+      case .active:
+        reverseButton.isEnabled = true
+        if viewAnimator.isRunning {
+          viewAnimator.pauseAnimation()
+          animationIsRunning = false
+          resetButton.isEnabled = true
+        } else {
+          viewAnimator.startAnimation()
+          reverseButton.isEnabled = !animatingBackwards
+          animationIsRunning = true
+          
+        }
+        
+      case .stopped:
+        viewAnimator.startAnimation()
+        animationIsRunning = true
+      }
+      
+    }
   }
   
   @IBAction func handleResetButton(_ sender: UIButton?) {
     animatingBackwards = false
     resetButton.isEnabled = false
-    viewAnimator.stopAnimation(true)
+    viewAnimator?.stopAnimation(true)
     reverseButton.isEnabled = false
     animationSlider.isEnabled = false
     animationIsRunning = false
-    centerConstraint.constant = 30
+    centerConstraint.constant = margin
+    UIView.animate(withDuration: 0.2){
+      self.view.layoutIfNeeded()
+    }
     animationSlider.value = 0.0
     view.layoutIfNeeded()
   }
